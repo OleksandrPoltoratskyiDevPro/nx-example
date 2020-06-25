77635f06e68538b372d6abe4767c1bdff7d45586
Poltoratskyi <oleksandr.poltoratskyi@dev.pro>
2020-06-25 15:16:56 +0300

    App init and default configuration

--- apps/ui-default/src/app/app.component.ts
+++ apps/ui-default/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Todo } from '@wp-default/data';
 
 @Component({
   selector: 'wp-default-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ui-default';
+  todos: Array<Todo> = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`,
+    });
+  }
 }
